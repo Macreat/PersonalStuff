58b40e107c580e4ea2c7135c38f4bf9cbb76a5d1
macreat <mateomacreat@gmail.com>
2026-02-20 17:43:00 -0500

    cCourse : about static variables

--- cCourse/static/static.c
+++ cCourse/static/static.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+While static variables have scope over the file containing them making them accessible only inside a given file,
+global variables can be accessed outside the file too.
+
+*/
 int runner()
 {
     static int count = 0; // if its not STATIC , and its global, the desire value its removed from memory, as soon as the function completes.
